4d858b4f479357071f7c5cc8e560e92da2b77d95
Serhii <1077stargonkiff7@gmail.com>
2023-11-21 15:59:38 +0200

    add SingIn test, and page

---
+++ src/test/java/tests/SignInTests/SignIn.java
@@ -0,0 +1,18 @@
+package tests.SignInTests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObject.pages.SignInPage.SignInPage;
+import tests.TestInit;
+
+public class SignIn extends TestInit {
+    @Test
+    public void signInPositive() {
+        SignInPage signIn = new SignInPage(getDriver());
+
+        signIn.login();
+
+        Assert.assertTrue(signIn.findByXpath("//div[text()='q']").isDisplayed());
+
+    }
+}

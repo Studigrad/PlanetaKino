0a6d6e49ce08bd1a325e80c71d11b433ff6edafe
Venusichek <venjasqa@gmail.com>
2023-11-21 17:00:47 +0200

    master

--- src/main/java/pageObject/pages/HomePage.java
+++ src/main/java/pageObject/pages/HomePage.java
@@ -1,10 +1,28 @@
 package pageObject.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import pageObject.BasePage;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 public class HomePage extends BasePage {
     public HomePage(WebDriver driver) {
         super(driver);
     }
+
+    public List<WebElement> filmlist() {
+        return findsByXpath("//div[@class='movie-block__poster-wr']");
+
+    }
+
+    public WebElement getElementFromFilmList(){
+        return filmlist().get(generateRandomNumber(0, filmlist().size()));
+    }
+
+
+
+
 }
